8938ddc3e69b11f8fb0635fd73b8ee7bda7b626f
Dillon Curry <dilloncurry@gmail.com>
2018-07-20 08:29:15 -0400

    Added the initial proof of work system

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,17 +1,22 @@
-const Block = require('./block');
+const Block = require("./block");
+const { DIFFICULTY } = require("../config.js");
 
-describe('Block', () => {
+describe("Block", () => {
   let data, lastBlock, block;
   beforeEach(() => {
-    data = 'bar';
+    data = "bar";
     lastBlock = Block.genesis();
     block = Block.mineBlock(lastBlock, data);
-  })
-  it('sets the `data` to match the input', () => {
+  });
+  it("sets the `data` to match the input", () => {
     expect(block.data).toEqual(data);
   });
 
-  it('sets the `lastHash` to match the hash of the last block', () => {
+  it("sets the `lastHash` to match the hash of the last block", () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
   });
-})
+
+  it("generates a hash that matches the difficulty", () => {
+    expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY))
+  });
+});
